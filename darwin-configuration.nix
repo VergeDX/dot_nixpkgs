f0b9fa2beaa0432460af0ebdedc7bac1af5e290d
Vanilla <neko@hydev.org>
2021-08-07 20:46:22 +0800

    [M] Do the first time of nixpkgs-fmt * command.

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -3,9 +3,7 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ pkgs.vim
-    ];
+  environment.systemPackages = [ pkgs.vim ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -16,7 +14,7 @@
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
