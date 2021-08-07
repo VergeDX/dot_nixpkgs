4b05c06e71a6f19098f0d8f28a8a69d0bf74e373
Vanilla <neko@hydev.org>
2021-08-07 21:16:15 +0800

    [-] Remove vim from system packages.

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -3,7 +3,7 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [ pkgs.vim ];
+  # environment.systemPackages = [ pkgs.vim ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
