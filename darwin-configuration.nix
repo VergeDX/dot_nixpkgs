2fe6ddee42e7844536622c4743d2c289f4839508
Vanilla <neko@hydev.org>
2021-08-08 21:55:45 +0800

    [M] Split large config out, make 2 nix file to contains.

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -35,63 +35,15 @@ in
     pkgs.powerline-symbols
   ];
 
-  # https://github.com/LnL7/nix-darwin/issues/139
-  system.activationScripts.applications.text = pkgs.lib.mkForce (
-    ''
-      echo "setting up ~/Applications/Nix..."
-
-      rm -rf ~/Applications/Nix && mkdir -p ~/Applications/Nix
-      chown ${me} ~/Applications/Nix
-
-      IFS='
-      '
-
-      for app in $(find ${config.system.build.applications}/Applications -maxdepth 1 -type l); do
-        src="$(/usr/bin/stat -f%Y "$app")" && appname="$(basename $src)"
-        osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${me}/Applications/Nix/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
-      done
-    ''
-  );
-
-  # https://github.com/NixOS/nix/issues/1669
-  # https://stackoverflow.com/a/26477515/166289
-  environment.launchDaemons."3_proxy_variables" = {
-    enable = true;
-    target = "3_proxy_variables.plist";
-    text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>environment</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>sh</string>
-            <string>-c</string>
-            <string>launchctl setenv https_proxy http://127.0.0.1:7890</string>
-            <string>launchctl setenv http_proxy http://127.0.0.1:7890</string>
-            <string>launchctl setenv all_proxy socks5://127.0.0.1:7890</string>
-            <!-- Reload nix-daemon after set proxy environment variable. -->
-            <string>launchctl kickstart -k system/org.nixos.nix-daemon.plist</string>
-          </array>
-          <key>KeepAlive</key>
-          <false/>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>WatchPaths</key>
-          <array>
-            <string>/etc/environment</string>
-          </array>
-        </dict>
-      </plist>
-    '';
-  };
-
   # https://nixos.wiki/wiki/Flakes
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = "experimental-features = nix-command flakes";
 
   # Package ‘vscode’ has an unfree license (‘unfree’), refusing to evaluate.
   nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    ./3_proxy_variables.nix
+    ./applications_fix.nix
+  ];
 }
