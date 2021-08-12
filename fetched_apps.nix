002842be13465b839adc91075f5cd282fc17b82d
Vanilla <neko@hydev.org>
2021-08-12 09:54:47 +0800

    [+] Finish build darwin apps script, support pkg | dmg | zip.

--- fetched_apps.nix
+++ fetched_apps.nix
@@ -1,31 +1,53 @@
 let
   pkgs = import <nixpkgs> { };
-  buildDarwinApps = name: version: link: sha256: extraComamnd: pkgs.stdenv.mkDerivation rec {
-    inherit name version;
-    isZip = if pkgs.lib.hasSuffix link ".zip" then true else false;
+  # https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.text/take-last.html
+  stringTakeLast =
+    str: last:
+    let strLen = pkgs.lib.stringLength str;
+    in pkgs.lib.substring (strLen - last) strLen str;
 
-    src = pkgs.fetchurl {
-      url = link;
-      sha256 = sha256;
+  buildDarwinApps = name: version: location: link: sha256:
+    let supportFmt = [ "dmg" "pkg" "zip" ]; in
+    pkgs.stdenv.mkDerivation rec {
+      inherit name version;
+
+      fileType = let extName = stringTakeLast link 3; in
+        if builtins.elem extName supportFmt then extName
+        else abort "Unsupported compression file types: ${extName}";
+
+      src = pkgs.fetchurl {
+        url = link;
+        sha256 = sha256;
+      };
+
+      nativeBuildInputs = [ pkgs.xar pkgs.cpio pkgs.unzip ];
+      # https://stackoverflow.com/questions/11298855/how-to-unpack-and-pack-pkg-file
+      unpackPhase =
+        if fileType == "dmg" then ''
+          /usr/bin/hdiutil attach ${src}
+          cd "/Volumes/${location}"
+        '' else if fileType == "pkg" then ''
+          xar -xf ${src} && cd ${location}
+          cat Payload | gunzip -dc | cpio -i
+        '' else if fileType == "zip" then "unzip ${src}"
+        else abort "No implement compress format: ${fileType}";
+
+      installPhase = ''
+        mkdir -p $out/Applications/
+        find . | grep ".app$" | head -n 1 | xargs -I {} cp -r {} $out/Applications/
+      '';
     };
-
-    nativeBuildInputs = [ pkgs.unzip pkgs.p7zip ];
-    unpackPhase = if isZip then "unzip ${src}" else "7z x ${src} || true";
-    installPhase = ''
-      mkdir -p $out/Applications/
-      # https://stackoverflow.com/questions/46021955/get-first-line-of-a-shell-commands-output
-      # https://stackoverflow.com/questions/1610089/how-to-use-cp-from-stdin
-      find . | grep "\.app" | head -n 1 | xargs -I {} cp -r {} $out/Applications/ || true
-    '' + "cd $out;" + extraComamnd;
-  };
 in
 [
-  # (mac_zip_apps "atom" "1.58.0"
+  # (buildDarwinApps "atom" "1.58.0" "Atom"
   #   "https://github.com/atom/atom/releases/download/v1.58.0/atom-mac.zip"
   #   "sha256-KNjPSH5oJx3ofgQn2LVmYJ4p1Avp/wp2wJ7HKIOo6DY=")
 
-  (buildDarwinApps "idea-ultimate" "2021.2"
-    "https://download.jetbrains.com/idea/ideaIU-2021.2.dmg"
-    "sha256-wj7p9oq71QPlAZx0XMW/KjCPgejCu9IQzPr7wRJMHlk="
-    "chmod +x './Applications/IntelliJ IDEA.app/Contents/MacOS/idea'")
+  # (buildDarwinApps "zoom-us" "5.7.4.898" "zoomus.pkg"
+  # "https://zoom.us/client/5.7.4.898/Zoom.pkg"
+  # "sha256-RLk3uw2bhQIov94qvncpaZFSjW3aSdf/ZRKlIirQIuY=")
+
+  # (buildDarwinApps "idea-ultimate" "2021.2" "IntelliJ IDEA"
+  #   "https://download.jetbrains.com/idea/ideaIU-2021.2.dmg"
+  #   "sha256-wj7p9oq71QPlAZx0XMW/KjCPgejCu9IQzPr7wRJMHlk=")
 ]
