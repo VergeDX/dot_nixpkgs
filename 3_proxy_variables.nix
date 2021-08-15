38e8b63f0507918127baae2bfe569fb064f171c5
Vanilla <neko@hydev.org>
2021-08-15 17:47:06 +0800

    [M] Fix daemon could not found issues, extra suffix detached.

--- 3_proxy_variables.nix
+++ 3_proxy_variables.nix
@@ -20,7 +20,7 @@
             <string>launchctl setenv http_proxy http://127.0.0.1:7890</string>
             <string>launchctl setenv all_proxy socks5://127.0.0.1:7890</string>
             <!-- Reload nix-daemon after set proxy environment variable. -->
-            <string>launchctl kickstart -k system/org.nixos.nix-daemon.plist</string>
+            <string>launchctl kickstart -k system/org.nixos.nix-daemon</string>
           </array>
           <key>KeepAlive</key>
           <false/>
